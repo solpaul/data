b856e8ad4b24d2ef61a673c2a28be67580fa4867
solpaul <sol.paul@gmail.com>
2017-03-30 18:19:53 +0100

    added team matrix functions

--- subreddit-algebra/subredditVectorAnalysis.r
+++ subreddit-algebra/subredditVectorAnalysis.r
@@ -56,7 +56,62 @@ findrelsubreddit <- function(cursubs,curops,numret=20) {
 return(head(curclosesubsorderc,numret))
 }
 
-## Political examples
+# Function to calculate similarity between two specified subreddits
+subredditsimilarity <- function(sub1,sub2) {
+	sub1 = tolower(sub1)
+	sub2 = tolower(sub2)
+	sub1vec = cursubmat[which(currownameslc==sub1),]
+	sub2vec = cursubmat[which(currownameslc==sub2),]
+	subsimilarity = cosine(x=sub1vec,y=sub2vec)
+return(subsimilarity)
+}
+
+## function to calculate similarity matrix for set of subreddits
+subredditmatrix <- function(cursubs) {
+	cursubs = tolower(cursubs)
+	similaritymatrix = matrix(0, ncol=length(cursubs), nrow=length(cursubs))
+	for(i in 1:length(cursubs)) {
+		for(j in 1:length(cursubs)) {
+			similaritymatrix[i,j] = subredditsimilarity(cursubs[i], cursubs[j])
+		}
+	}
+	rownames(similaritymatrix) = cursubs
+	colnames(similaritymatrix) = cursubs
+return(similaritymatrix)
+}
+
+## matrix of similarities between premier league clubs (Middlesbrough did not have enough comments)
+premierleagueclubs <- c("Gunners", "AFCBournemouth", "chelseafc", "crystalpalace", "Everton", 
+		       "HullCity", "lcfc", "LiverpoolFC", "MCFC", "reddevils", "SaintsFC", 
+		       "StokeCityFC", "safc", "coys", "Watford_FC", "WBAfootball", "Hammers", 
+		       "swanseacity", "Burnley")
+subredditmatrix(premierleagueclubs)
+
+## matrix of similarities between nfl teams (St Louis Rams moved to LA)
+nflteams <- c("buffalobills", "ravens", "Colts", "DenverBroncos", "miamidolphins", "bengals", 
+		   "Jaguars", "KansasCityChiefs", "Patriots", "Browns", "Texans", "oaklandraiders", 
+		   "nyjets", "steelers", "Tennesseetitans", "Chargers", "cowboys", "CHIBears", "falcons", 
+		   "AZCardinals", "NYGiants", "detroitlions", "panthers", "49ers", "eagles", 
+		   "GreenBayPackers", "Saints", "Seahawks", "Redskins", "minnesotavikings", "buccaneers", 
+		   "StLouisRams", "LosAngelesRams")
+subredditmatrix(nflteams)
+
+## matrix of similarities between nba teams
+nbateams <- c("bostonceltics", "chicagobulls", "AtlantaHawks", "GoNets", "clevelandcavs", "CharlotteHornets", 
+	      "NYKnicks", "DetroitPistons", "heat", "sixers", "pacers", "OrlandoMagic", "torontoraptors", 
+	      "MkeBucks", "washingtonwizards", "denvernuggets", "warriors", "Mavericks", "timberwolves", 
+	      "LAClippers", "rockets", "Thunder", "lakers", "memphisgrizzlies", "ripcity", "suns", "NOLAPelicans", 
+	      "UtahJazz", "kings", "NBASpurs")
+subredditmatrix(nbateams)
+
+## matrix of similarities between mlb teams
+mlbteams <- c("angelsbaseball", "whitesox", "orioles", "Astros", "WahoosTipi", "redsox", "OaklandAthletics", 
+	      "motorcitykitties", "NYYankees", "Mariners", "KCRoyals", "tampabayrays", "TexasRangers", 
+	      "minnesotatwins", "Torontobluejays", "azdiamondbacks", "Cubs", "Braves", "ColoradoRockies", 
+	      "Reds", "letsgofish", "Dodgers", "Brewers", "NewYorkMets", "Padres", "buccos", "phillies", 
+	      "SFGiants", "Cardinals", "Nationals")
+subredditmatrix(mlbteams)
+
 
 # /r/The_Donald
 cursubs = c("the_donald")
